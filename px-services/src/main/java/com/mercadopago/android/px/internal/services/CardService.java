084eb2964cd0c4a1bf40852a87f9cf42b8061641
Serguei <serguei.outenkov@mercadolibre.com>
2018-09-25 09:47:18 -0300

    fix url

--- px-services/src/main/java/com/mercadopago/android/px/internal/services/CardService.java
+++ px-services/src/main/java/com/mercadopago/android/px/internal/services/CardService.java
@@ -9,7 +9,7 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface CardService {
-    @POST("/{version}/px_mobile_api/card_association")
+    @POST("/{version}/px_mobile_api/card-association")
     MPCall<Card> assignCard(@Path(value = "version", encoded = true) String version,
         @Query("access_token") String accessToken, @Body HashMap<String, Object> body);
 }
